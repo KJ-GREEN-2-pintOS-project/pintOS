414e47ca7c8a4e6608a84fb0b72409ed36d423da
kelvin3476 <winrain03226@gmail.com>
2023-05-30 16:16:42 +0900

    Priority thrid draft

--- threads/synch.c
+++ threads/synch.c
@@ -67,7 +67,7 @@ sema_down (struct semaphore *sema) {
 	old_level = intr_disable ();
 	while (sema->value == 0) {
 		// list_push_back (&sema->waiters, &thread_current ()->elem);
-		list_insert_ordered (&sema->waiters, &thread_current()->elem, thread_compare_priority, 0);
+		list_insert_ordered (&sema->waiters, &thread_current ()->elem, thread_compare_priority, 0);
 		thread_block ();
 	}
 	sema->value--;
@@ -192,7 +192,17 @@ lock_acquire (struct lock *lock) {
 	ASSERT (!intr_context ());
 	ASSERT (!lock_held_by_current_thread (lock));
 
+	struct thread *curr = thread_current();
+	if (lock->holder) {
+		curr->wait_on_lock = lock;
+		list_insert_ordered(&lock->holder->donations, &curr->donation_elem, thread_compare_donate_priority, 0);
+
+		donate_priority();
+	}
+
 	sema_down (&lock->semaphore);
+
+	curr->wait_on_lock = NULL;
 	lock->holder = thread_current ();
 }
 
@@ -225,6 +235,9 @@ void
 lock_release (struct lock *lock) {
 	ASSERT (lock != NULL);
 	ASSERT (lock_held_by_current_thread (lock));
+
+	remove_with_lock(lock);
+	refresh_priority();
 
 	lock->holder = NULL;
 	sema_up (&lock->semaphore);
